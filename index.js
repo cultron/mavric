a85fdd3805c9ce9df5c569b66c7e1fc6fa2776ec
Marconi Moreto Jr <git@marconijr.com>
2017-07-27 12:03:34 +0800

    added Cache configurator

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 //All core configurators
 const Database = require('./configurators/database');
+const Cache = require('./configurators/cache');
 const HttpController = require('./configurators/http-controller');
 const Helper = require('./configurators/helpers');
 
@@ -8,7 +9,8 @@ module.exports = {
         Helper: Helper.Configurator,
         Tracker: require('./configurators/tracker'),
         HttpController: HttpController.Configurator,
-        Database: Database.Configurator
+        Database: Database.Configurator,
+        Cache: Cache
     },
     HttpServer: require('./configurators/http-server'),
     Database: Database,
