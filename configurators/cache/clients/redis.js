e046ec78010b4c979d68c0e906b9b08e15c5ab35
Matt Cullerton <matt@nativeaxis.com>
2017-08-04 10:52:20 -0700

    adding password support for redis

--- configurators/cache/clients/redis.js
+++ configurators/cache/clients/redis.js
@@ -14,9 +14,14 @@ module.exports = (container, callback) => {
         const log = container.resolveSync('Log');
         const port = config.redis.port;
         const host = config.redis.host;
+        const password = config.redis.password; //optional
 
         log.info(`Connecting to redis on ${chalk.cyan(`${host}:${port}`)}`);
-        const redisClient = redis.createClient(port, host);
+        const redisClient = redis.createClient({
+            host,
+            port,
+            password
+        });
         redisClient.on('error', (err) => {
             log.error(err);
             process.exit(1); //this may be too harsh, but if your app depends on redis to run, its appropriate.
