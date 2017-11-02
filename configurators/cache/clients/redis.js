784e221d9cce366db78b528624b1c4b4c1da5715
Matt Cullerton <matt@nativeaxis.com>
2017-11-02 10:42:38 -0700

    removing xml2json; adding retry strategy to redis

--- configurators/cache/clients/redis.js
+++ configurators/cache/clients/redis.js
@@ -16,14 +16,34 @@ module.exports = (container, callback) => {
         const host = config.redis.host;
         const password = config.redis.password; //optional
 
+        const reconnectStrategy = (options) => {
+            if (options.error && options.error.code === 'ECONNREFUSED') {
+                // End reconnecting on a specific error and flush all commands with
+                // a individual error
+                return new Error('The server refused the connection');
+            }
+            if (options.total_retry_time > 1000 * 60 * 60) {
+                // End reconnecting after a specific timeout and flush all commands
+                // with a individual error
+                return new Error('Retry time exhausted');
+            }
+            if (options.attempt > 10) {
+                // End reconnecting with built in error
+                return undefined;
+            }
+            // reconnect after
+            return Math.min(options.attempt * 100, 3000);
+        };
+
         log.info(`Connecting to redis on ${chalk.cyan(`${host}:${port}`)}`);
         const redisClient = redis.createClient({
             host,
             port,
-            password
+            password,
+            retry_strategy: reconnectStrategy
         });
         redisClient.on('error', (err) => {
-            log.error(err);
+            log.error('Redis Error:', err);
             process.exit(1); //this may be too harsh, but if your app depends on redis to run, its appropriate.
         });
 
